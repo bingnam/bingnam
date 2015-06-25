afa2d4cd32e7b32ddfbf648cea91aa094834b2cd
bingnam <bingnam@users.noreply.github.com>
2015-06-25 18:50:28 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,21 +1,51 @@
-var fs = require('fs');
+ var fs = require('fs');
 
-function run(gen) {
-  var gen_obj = gen(resume);
-  function resume() {
-    gen_obj.next(arguments);
-  }
-  gen_obj.next();
-}
+ 
 
-run(function* gen(resume) {
-  var ret, err, data;
-  ret = yield fs.readFile('./test.txt', resume);
-  err = ret[0];
-  data = ret[1];
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
-});
+ function run(gen) {
+
+   var gen_obj = gen(resume);
+
+   function resume() {
+
+     var err = arguments[0];
+
+     if (err && err instanceof Error) {
+
+       return gen_obj.throw(err);
+
+     }
+
+     var data = arguments[1];
+
+     gen_obj.next(data);
+
+   }
+
+   gen_obj.next();
+
+ }
+
+ 
+
+ run(function* gen(resume) {
+
+   var ret;
+
+   try {
+
+     ret = yield fs.readFile('./test.txt', resume);
+
+     console.log(ret);
+
+   } catch (e) {
+
+     console.log(e);
+
+   } finally {
+
+     console.log('finally');
+
+   }
+
+ });
